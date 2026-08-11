b8cbbe441674d0db45f0f968350e04d5d80d4532
Jim97M <wabuyajames@gmail.com>
2026-08-11 05:00:38 +0300

    Feat: Add Notif Service and Portal Management

---
+++ portal-management/src/main/java/com/portal_management/PortalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.portal_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalManagementApplication.class, args);
+	}
+
+}
---
+++ portal-management/src/test/java/com/portal_management/PortalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
